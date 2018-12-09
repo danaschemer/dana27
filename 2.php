a9fcfa4b5d69d9d882d83c9cd7f9cde3059726f9
danaschemer <amahendra7@gmail.com>
2018-12-09 21:55:20 +0700

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,10 @@
+<?php
+///Jangan hapus tanpa kutip nya(")
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+///Ganti xxxxxxx dengan punya kalian
+///Capture dulu apk nya ambil yang di butuhkan
+$ticket = 
+"xxxxx";
+$meid = "xxxx";
+$os="android";
+$sign="xxxx";
