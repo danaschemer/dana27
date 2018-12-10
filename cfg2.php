d801ef67f15e2d963baf740e97727b7c367e1763
danaschemer <amahendra7@gmail.com>
2018-12-11 02:20:04 +0700

    Update cfg2.php

--- cfg2.php
+++ cfg2.php
@@ -3,8 +3,7 @@
 ///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
 ///Ganti xxxxxxx dengan punya kalian
 ///Capture dulu apk nya ambil yang di butuhkan
-$ticket = 
-"xxxxx";
-$meid = "xxxx";
-$os="android";
-$sign="xxxx";
+$ticket = "xxxxxxxxxxxxxxxxxx";
+$meid = "xxxxxxxxxxxxxxxxxx";
+$os="xxxxxxxxxxxxxxxxxx";
+$sign="xxxxxxxxxxxxxxxxxx";
