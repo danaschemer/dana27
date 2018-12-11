548748c2dab67012f257cfa001af67e8f138c0a1
danaschemer <amahendra7@gmail.com>
2018-12-11 10:15:13 +0700

    Delete cfg2.php

--- cfg2.php
+++
@@ -1,9 +0,0 @@
-<?php
-///Jangan hapus tanpa kutip nya(")
-///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
-///Ganti xxxxxxx dengan punya kalian
-///Capture dulu apk nya ambil yang di butuhkan
-$ticket = "xxxxxxxxxxxxxxxxxx";
-$meid = "xxxxxxxxxxxxxxxxxx";
-$os="xxxxxxxxxxxxxxxxxx";
-$sign="xxxxxxxxxxxxxxxxxx";
